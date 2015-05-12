cf2f9d286d8119e8792515dc8d4a72753910b097
jlburkhead <jlburkhead@ucdavis.edu>
2015-05-11 17:05:29 -0700

    add "async" version

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 var LRU = require("lru-cache");
 
+function nullFn() {};
+
 function Cache(options) {
   this._storage = LRU(options);
 }
 
-Cache.prototype.set = function(key, value, groups) {
-  key = this._make_key(key, groups);
-  this._storage.set(key, value);
+Cache.prototype.set = function(key, value, groups, callback) {
+  var storage = this._storage;
+  callback = callback || nullFn;
+  if (callback === undefined) {
+    callback = groups;
+    groups = null;
+  }
+  this._make_key(key, groups, function(err, key) {
+    if (err) { return callback(err); }
+    storage.set(key, value);
+    console.log("SET", key, value);
+    callback(null);
+  });
 };
 
-Cache.prototype.get = function(key, groups) {
-  var result;
-  key = this._make_key(key, groups);
-  result = this._storage.get(key);
-  return result === undefined ? null : result;
+Cache.prototype.get = function(key, groups, callback) {
+  var storage = this._storage;
+  callback = callback || nullFn;
+  if (callback === undefined) {
+    callback = groups;
+    groups = null;
+  }
+  this._make_key(key, groups, function(err, key) {
+    if (err) { return callback(err); }
+    process.nextTick(function() {
+      var result = storage.get(key);
+      console.log("GET", key);
+      callback(null, result === undefined ? null : result);
+    });
+  });
 };
 
-Cache.prototype.invalidateGroup = function(group) {
-  this._storage.set(group, (this._storage.get(group) || 0) + 1);
+Cache.prototype.invalidateGroup = function(group, callback) {
+  var generation = (this._storage.get(group) || 0) + 1;
+  callback = callback || nullFn;
+  this._storage.set(group, generation);
+  console.log("SET", group, generation);
+  callback(null);
 };
 
-
-Cache.prototype._make_key = function(key, groups) {
+Cache.prototype._make_key = function(key, groups, callback) {
   var storage = this._storage;
   if (!groups) {
-    return key;
+    return callback(null, key);
   }
   if (!Array.isArray(groups)) {
     groups = [groups];
@@ -34,7 +59,7 @@ Cache.prototype._make_key = function(key, groups) {
     memo += ':' + item + '_'  + generation;
     return memo;
   }, '');
-  return key + groups;
+  callback(null, key + groups);
 }
 
 module.exports = Cache;
